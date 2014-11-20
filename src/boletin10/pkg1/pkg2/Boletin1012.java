661185a6bfd292a7914b2b40415554f8c16ea4ca
rfernandezgonzalez <rfernandezgonzalez@eniac04.danielcastelao.org>
2014-11-20 11:39:42 +0100

    (1.0v)

---
+++ src/boletin10/pkg1/pkg2/Boletin1012.java
@@ -0,0 +1,14 @@
+package boletin10.pkg1.pkg2;
+
+public class Boletin1012 {
+
+    public static void main(String[] args) {
+
+        Juego obj1 = new Juego();
+
+        obj1.Recoger();
+        obj1.Evaluar();
+
+    }
+
+}
